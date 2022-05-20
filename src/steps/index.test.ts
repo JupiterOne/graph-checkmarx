13d291033c0c0172bb6e161c1027895b712b2e11
Austin Kelleher <austinlkelleher@gmail.com>
2022-05-19 20:18:48 -0400

    Small workaround to date parsing issue

--- src/steps/index.test.ts
+++ src/steps/index.test.ts
@@ -59,7 +59,14 @@ describe('Checkmarx', () => {
     expect({
       numCollectedEntities: context.jobState.collectedEntities.length,
       numCollectedRelationships: context.jobState.collectedRelationships.length,
-      collectedEntities: context.jobState.collectedEntities,
+      collectedEntities: context.jobState.collectedEntities.map((e) => ({
+        ...e,
+        // HACK (austinkelleher): It looks like the formatting that Checkmarkx
+        // uses does not include the timezone and it's causing issues with tests
+        //
+        // See a failing SDK test here: https://github.com/JupiterOne/sdk/commit/d88453dbf87e04802b6e81a398123b10e1a344f0#diff-eb9509d3689a5d1dde0aaf6a4f037d78ee0c178b48400ccc3249d8b84f726328R155
+        createdOn: 123456789,
+      })),
       collectedRelationships: context.jobState.collectedRelationships,
       encounteredTypes: context.jobState.encounteredTypes,
     }).toMatchSnapshot();
